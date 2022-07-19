f9a7e30a431d1204a67082d554db7c520d606858
Nathan Ueda <nateueda@gmail.com>
2022-07-18 19:22:36 -0700

    Added stack.py and test_stack.py.

---
+++ Elementary Data Structures/stack.py
@@ -0,0 +1,59 @@
+"""
+Stack ADT Operations:
+1. pop(): Remove the top item from the stack.
+2. push(item): Add an item to the top of the stack.
+3. top(): Return the top of the stack.
+4. is_empty(): Return true if and only if the stack is empty.
+"""
+
+"""
+Time Complexity:
+
+"""
+
+class Stack:
+    """Implementation of the stack ADT using a list."""
+
+    def __init__(self):
+        """Create an empty stack."""
+        self.stack = []
+    
+    def is_empty(self):
+        """
+        Checks to see if the stack is empty.
+        @return: True if the stack is empty, false otherwise.
+        """
+        return len(self.stack) == 0
+
+    def pop(self):
+        """
+        Removes the top item from the stack.
+        @return: The element popped from the stack.
+        """
+        if self.is_empty():
+            raise Exception("Error: Cannot pop an empty stack.")
+        else:
+            return self.stack.pop()
+
+    def top(self):
+        """
+        Peeks at the top item of the stack without popping it.
+        @return: The element on top of the stack.
+        """
+        if self.is_empty():
+            raise Exception("Error: Stack is empty.")
+        else:
+            return self.stack[-1]
+    
+    def push(self, item):
+        """
+        Adds an item to the top of the stack.
+        @param: The item to end to the top of the stack.
+        """
+        self.stack.append(item)
+
+"""
+Notes:
+- Uses LIFO (last-in, first-out) ordering.
+- Data can only be inserted, deleted, and accessed at the end of a stack.
+"""
